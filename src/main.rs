8adbfd1a7fcb03f8bbdcb3adac66855be5fa886f
Hao Tran <haoanhtran7@gmail.com>
2024-09-04 13:12:19 -0700

    FEAT: External works

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,7 @@ fn main() {
 
     let handle_scope = &mut v8::HandleScope::new(isolate);
     let context: v8::Local<v8::Context> = v8::Context::new(handle_scope);
+    let persistent_context = v8::Global::new(handle_scope, context);
     let global = context.global(handle_scope);
     let scope = &mut v8::ContextScope::new(handle_scope, context);
 
@@ -57,26 +58,41 @@ fn main() {
     let base_file_object = FS::new("blob.txt", "./src/example/blob", "read");
 
     //Rc<RefCell<>>
+    //Rc is a reference counted pointer and is single threaded
+    //Arc async reference counted and used with async code
+    //Both Rc and Arc are smart pointers and go out of scope after the last reference, they are read only by default 
+    //RefCell is a 'container' which allows us to mutate the smart pointer
     let fs_object = Rc::new(RefCell::new(base_file_object));
     let fs_object_c_pointer = Rc::as_ptr(&fs_object) as *mut c_void;
     let external_fs = v8::External::new(scope, fs_object_c_pointer);
 
+    // Create an ObjectTemplate with internal fields
+    let object_template = v8::ObjectTemplate::new(scope);
+    object_template.set_internal_field_count(1);
+
+    // Create a new Object from the template
+    let object = object_template.new_instance(scope).unwrap();
+    
+    // Set the internal field
+    object.set_internal_field(0, external_fs.into());
+
+    // Create persistent handle for object
+    let persistent_object = v8::Global::new(scope, object);
+
+    // Add the object to the global scope
+    let key = v8::String::new(scope, "FS").unwrap();
+    global.set(scope, key.into(), object.into());
+
     // Register the function in V8
     let fn_template = v8::FunctionTemplate::new(scope, get_fs_info_callback);
     let function = fn_template.get_function(scope).unwrap();
-    fn_template.prototype_template(scope).set_internal_field_count(1);
-    function.set_internal_field(0, external_fs.into());
-
-    // Attach the function to the global object
-    let global = context.global(scope);
     let key = v8::String::new(scope, "getFsInfo").unwrap();
     global.set(scope, key.into(), function.into());
 
-    // Register the function in V8
+    // Execute the JavaScript code to call getFsInfo
     let js_code = v8::String::new(scope, "getFsInfo();").unwrap();
     let script = v8::Script::compile(scope, js_code, None).unwrap();
     script.run(scope).unwrap();
-
 }
 
 pub fn get_fs_info_callback(
@@ -84,10 +100,35 @@ pub fn get_fs_info_callback(
     args: v8::FunctionCallbackArguments, 
     _return_object: v8::ReturnValue 
 ) {
-    // Get the `this` object (the function object) and access the internal field
-    let this = args.this();
-    let external = this.get_internal_field(handle_scope, 0).unwrap();
-    let external = v8::Local::<v8::External>::try_from(external).unwrap();
+    // Get the current context from the handle scope
+    let context = handle_scope.get_current_context();
+
+    // Access the global object of the current context
+    let global = context.global(handle_scope);
+
+    // Retrieve the object with the internal field from the global object
+    let key = v8::String::new(handle_scope, "FS").unwrap();
+    let object = global.get(handle_scope, key.into()).unwrap();
+
+    // Ensure the retrieved value is the expected object with an internal field
+    if !object.is_object() {
+        eprintln!("Error: fs_internal is not an object");
+        return;
+    }
+
+    let object = v8::Local::<v8::Object>::try_from(object).unwrap();
+
+    // Access the internal field
+    let internal_field = object.get_internal_field(handle_scope, 0);
+
+    // Handle the case where the internal field might be None
+    let external = match internal_field {
+        Some(field) => v8::Local::<v8::External>::try_from(field).unwrap(),
+        None => {
+            eprintln!("Error: No internal field set on the object");
+            return;
+        }
+    };
 
     let raw_ptr = external.value() as *const RefCell<FS>;
 
