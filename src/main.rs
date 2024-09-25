b90167aea4b791b4c9354bf9909072a38221672e
Hao Tran <haoanhtran7@gmail.com>
2024-09-24 18:03:36 -0700

    compile time and runtime errors handled

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ mod fs;
 fn main() {
     startup_v8_platform();
 
+    let isolate: &mut v8::OwnedIsolate = &mut v8::Isolate::new(Default::default()); 
 
     {
-        let isolate: &mut v8::OwnedIsolate = &mut v8::Isolate::new(Default::default()); 
 
         let handle_scope = &mut v8::HandleScope::new(isolate);
         let context: v8::Local<v8::Context> = v8::Context::new(handle_scope);
@@ -46,18 +46,40 @@ fn main() {
 
         //EXECUTE CODE
         let code = v8::String::new(scope, &file_contents).unwrap(); 
-        let script = v8::Script::compile(scope, code, None).unwrap();
-        let result = script.run(scope).unwrap();
-        let result = result.to_string(scope).unwrap();
+        let tc = &mut v8::TryCatch::new(scope); //Note TryCatch::new() -> Handle Scope or Context Scope
+        let maybe_script = v8::Script::compile(tc, code, None);
+
+        if let Some(script) = maybe_script {
+            let result = script.run(tc);
+
+            if result.is_none() && tc.has_caught() {
+                let exception = tc.exception().unwrap();
+                let exception_str = exception.to_string(tc).unwrap();
+                let msg = exception_str.to_rust_string_lossy(tc);
+                println!("Runtime Error: {}", msg);
+
+            }
+
+        } else if tc.has_caught(){
+            let exception = tc.exception().unwrap();
+            let exception_str = exception.to_string(tc).unwrap();
+            let msg = exception_str.to_rust_string_lossy(tc);
+            println!("Compile-Time Error: {}", msg);
+        }
+        // let result = result.to_string(scope).unwrap();
+
 
     }
 
     // DISPOSE V8 RESOURCES
-    unsafe{
-        v8::V8::dispose();
-    }
+    // unsafe{
+    //     v8::V8::dispose();
+    // }
 
-    v8::V8::shutdown_platform();
+    // All isolates should be out of scope before this method is called
+    // Generally disposal happens automatically
+
+    // v8::V8::shutdown_platform();
 }
 
 fn startup_v8_platform(){
@@ -66,3 +88,15 @@ fn startup_v8_platform(){
     v8::V8::initialize_platform(platform);
     v8::V8::initialize();
 }
+
+//Seems like try catch comes after a script is run or a module is compiled 
+//Otherwise conflict with mutable borrow
+//let try_catch = &mut v8::TryCatch::new(scope);
+
+//script_compiler API has slightly move advanced compilation features
+
+//local handles can created either within the context scope or handle scope depending whether you want to tie a context to the handle or not
+//will the local handle interact with Javascript? 
+
+//Rust -> Javascript (requires ContextScope)
+//Javascript -> Rust (HandleScope is more optimal)
